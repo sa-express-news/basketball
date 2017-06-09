20488d1e12bb3528f115b3de8243bd13fab39f90
Kia Farhang <kfarhang0@gmail.com>
2017-06-09 13:46:15 -0500

    Committing before trying to remove SkinnyContainer from app flow

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react';
 
-import StoryTop from '../StoryTop/StoryTop';
+import StoryTopContainer from '../StoryTopContainer/StoryTopContainer';
 import IntroParagraph from '../IntroParagraph/IntroParagraph';
 import SkinnyContainer from '../SkinnyContainer/SkinnyContainer';
 import Sections from '../Sections/Sections';
@@ -14,10 +14,12 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <SkinnyContainer width={90}>
-          <StoryTop data={Story.top} />
+        <SkinnyContainer>
+          <StoryTopContainer data={Story.top} />
+          <div className='Story'>
           <IntroParagraph text='On Feb. 2, around a quarter to 5 in the afternoon, just as the day began to darken, police officers approached four young men outside a recording studio in the Rose des Vents, an agglomeration of chalky cites, , or public-housing projects, that covers a remote expanse of the northeast Paris suburb Aulnay-sous-Bois. The police asked to see one man’s identity papers. Within minutes, another was on his stomach. The officers put handcuffs on the 21-year-old, whose family, of Congolese origin, lived nearby. When they arrived at the station, staff members noticed that he was in a lot of pain. He was rushed to a hospital, where an examination revealed a 10-centimeter tear in his rectum. It had been caused by a police baton.' />
           <Sections sections={Story.sections}/>
+          </div>
         </SkinnyContainer> 
       </div>
     );
