a0d719366b589293974bbd4fe4c3228bab4130ab
Kia Farhang <kfarhang0@gmail.com>
2017-06-21 14:21:12 -0500

    Added

--- src/components/ComponentMapper/ComponentMapper.jsx
+++ src/components/ComponentMapper/ComponentMapper.jsx
@@ -3,15 +3,20 @@
 import React from 'react';
 
 import Paragraph from '../Paragraph/Paragraph';
+import SectionHeading from '../SectionHeading/SectionHeading';
 import FullPhoto from '../FullPhoto/FullPhoto';
 import FullPhotoContainer from '../FullPhotoContainer/FullPhotoContainer';
 import SmallPhotoDesktop from '../SmallPhotoDesktop/SmallPhotoDesktop';
 import SmallPhotoDesktopContainer from '../SmallPhotoDesktopContainer/SmallPhotoDesktopContainer';
+import SmallPhotoDesktopContainerReverse from '../SmallPhotoDesktopContainerReverse/SmallPhotoDesktopContainerReverse';
 import PullQuote from '../PullQuote/PullQuote';
 import RelatedContent from '../RelatedContent/RelatedContent';
+import iFrame from '../iFrame/iFrame';
 
 export default{
 	text: (object: Object, key: number) => <Paragraph text={object.value} key={key}/>,
+
+	subhead: (object: Object, key: number) => <SectionHeading text={object.value} key={key}/>,
 
 	photo: function(object: Object, key: number){
 		const photo = object.value;
@@ -22,10 +27,14 @@ export default{
 			let photoComponent = <FullPhoto src={photoPath} alt={photo.caption}/>;
 			return <FullPhotoContainer caption={photo.caption} cutline={photo.cutline} key={key}>{photoComponent}</FullPhotoContainer>
 			
-			case 'small':
+			case 'small-left':
 			let smallPhotoComponent = <SmallPhotoDesktop src={photoPath} alt={photo.caption}/>
 			return <SmallPhotoDesktopContainer caption={photo.caption} cutline={photo.cutline} key={key}>{smallPhotoComponent}</SmallPhotoDesktopContainer>
 			
+			case 'small-right':
+			let smallPhotoComponentReverse = <SmallPhotoDesktop src={photoPath} alt={photo.caption}/>
+			return <SmallPhotoDesktopContainerReverse caption={photo.caption} cutline={photo.cutline} key={key}>{smallPhotoComponentReverse}</SmallPhotoDesktopContainerReverse>
+
 			default:
 			return null
 		};
@@ -38,6 +47,8 @@ export default{
 	pullquote: (object: Object, key: number) => <PullQuote quote={object.value.quote} key={key} />,
 
 	related: (object: Object, key: number) => <RelatedContent headline={object.value.headline} link={object.value.link} key={key} />,
+
+	iframe: (object: Object, key: number) => <iFrame src={object.value.source}/>,
 
 	renderComponent: function(object: Object, key: number){
 		return object.type && this[object.type] ? this[object.type](object, key) : null;
