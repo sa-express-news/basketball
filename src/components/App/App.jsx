01990958c02f2e2fa4190d2a3eb5a6d55923a1e1
Kia Farhang <kfarhang0@gmail.com>
2017-06-21 16:06:27 -0500

    Added lead graph to story

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -16,7 +16,7 @@ class App extends Component {
       <div className='App'>
           <StoryTopContainer data={Story.top} />
           <div className='Story'>
-          <IntroParagraph text='On Feb. 2, around a quarter to 5 in the afternoon, just as the day began to darken, police officers approached four young men outside a recording studio in the Rose des Vents, an agglomeration of chalky cites, , or public-housing projects, that covers a remote expanse of the northeast Paris suburb Aulnay-sous-Bois. The police asked to see one man’s identity papers. Within minutes, another was on his stomach. The officers put handcuffs on the 21-year-old, whose family, of Congolese origin, lived nearby. When they arrived at the station, staff members noticed that he was in a lot of pain. He was rushed to a hospital, where an examination revealed a 10-centimeter tear in his rectum. It had been caused by a police baton.' />
+          <IntroParagraph text='Towering above his high school defenders, 6-foot-10 center Charles Bassey took the ball on the right wing of the basket, dribbled, then flung it to the backboard in the playful style of a Harlem Globetrotter.' />
           <Sections sections={Story.sections}/>
           <Video src='https://s3.amazonaws.com/projects.expressnews.com/basketball/st-anthony.mp4' autoPlay={false} muted={true} loop={false}/>
           </div>
