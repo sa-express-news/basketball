eb1fa1aec85d10815a7b99ce367c531b2efaf3e8
Luke Whyte <lukeawhyte@gmail.com>
2017-06-23 15:18:33 -0500

    build social icons and block

--- src/components/StoryTopContainer/StoryTopContainer.jsx
+++ src/components/StoryTopContainer/StoryTopContainer.jsx
@@ -4,7 +4,7 @@ import React, {Component} from 'react';
 
 import StoryTop from '../StoryTop/StoryTop';
 import StoryTopDesktop from '../StoryTopDesktop/StoryTopDesktop';
-import ExpressNewsLogo from '../Icons/ExpressNewsLogo/ExpressNewsLogo';
+import SocialBlock from '../SocialBlock/SocialBlock';
 
 class StoryTopContainer extends Component{
 	props: {
@@ -13,30 +13,30 @@ class StoryTopContainer extends Component{
 
 	state = {
 		isDesktopScreen: false
-  	};
+  };
 
-  	componentDidMount(){
-  		this.setIsDesktop();
-    	window.addEventListener('resize', this.setIsDesktop);
+  componentDidMount(){
+  	this.setIsDesktop();
+    window.addEventListener('resize', this.setIsDesktop);
  	}
 
-  	setIsDesktop = () => {
-    	const isDesktopScreen = window.innerWidth > 1023;
-    	if (isDesktopScreen !== this.state.isDesktopScreen) {
-      		this.setState({ isDesktopScreen });
-    	}
-  	}
+  setIsDesktop = () => {
+    const isDesktopScreen = window.innerWidth > 1023;
+    if (isDesktopScreen !== this.state.isDesktopScreen) {
+      this.setState({ isDesktopScreen });
+    }
+  }
 
-  	render(){
-      const isDesktopScreen = this.state.isDesktopScreen;
-  		return(
-        <div>
-        <ExpressNewsLogo />
+  render(){
+    const isDesktopScreen = this.state.isDesktopScreen;
+  	return(
+      <div>
+        <SocialBlock />
   			{isDesktopScreen && <StoryTopDesktop data={this.props.data} />}
         {!isDesktopScreen && <StoryTop data={this.props.data} />}
-        </div>
-  		)
-  	}
+      </div>
+  	)
+  }
 }
 
 export default StoryTopContainer
